dc6e866ce5090ef5bace26d76795e8524c057b3a
aisl1n <aislanarchy@gmail.com>
2024-08-15 21:04:08 -0300

    chore: add next auth env variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ const Home = async () => {
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
-            src="/banner-01.png"
+            src="/banner-01.svg"
             fill
             className="rounded-xl object-cover"
           />
